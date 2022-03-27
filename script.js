f3e2d01a7dc4ae723fcc6b12663951a14739b8a3
Shannon <srosenb@protonmail.ch>
2022-03-27 05:23:02 -0500

    Pull out toggle script into function

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 // linkdrop
 
+function hideToggle(item) {
+    let itemHide = item.classList.contains('hide');
+    if (itemHide === false) {
+        item.classList.add('hide')
+        console.log(item.classList);
+    } else if (itemHide === true) {
+        item.classList.remove('hide');
+        console.log(item.classList);
+    }
+}
+
 let linkDrop = document.querySelector('.linkdrop img');
 let header1 = document.querySelector('.header1');
 let nav = document.querySelector('nav');
-// console.log({linkDrop});
-// console.log({header1});
-// console.log({nav});
 
 linkDrop.addEventListener('click', () => {
     console.log(nav.classList);
-    let navHide = nav.classList.contains('hide');
-    if (navHide === false) {
-        nav.classList.add('hide')
-        console.log(nav.classList);
-    } else if (navHide === true) {
-        nav.classList.remove('hide');
-        console.log(nav.classList);
-    }
-})
+    hideToggle(nav);
+});
 
 
 
