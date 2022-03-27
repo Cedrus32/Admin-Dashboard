1db3ba24e3c987a8a21d95de9138eecdd9f6a404
Shannon <srosenb@protonmail.ch>
2022-03-27 16:41:06 -0500

    Fix section header height/width change on exp/coll

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-// functions
+// ------------- //
+// NAV HIDE/SHOW //
+// ------------- //
 
-function navHideToggle(item1, item2) {
-    let itemHide = item1.classList.contains('hide');
-    if (itemHide === false) {
-        item2.classList.add('hide')
-    } else if (itemHide === true) {
-        item2.classList.remove('hide');
+function navHideToggle(item, hide) {
+    if (hide === false) {
+        item.classList.add('hide')
+    } else if (hide === true) {
+        item.classList.remove('hide');
     }
 }
 
-function radiusToggle(item1, item2) {
-    let itemHide = item1.classList.contains('hide');
-    if (itemHide === false) {
-        item2.style.borderRadius = 'var(--ui-rad) var(--ui-rad) 0px 0px';
-    } else if (itemHide === true) {
+function headerRadToggle(item2, hide) {
+    if (hide === false) {
         item2.style.borderRadius = 'var(--ui-rad)';
+    } else if (hide === true) {
+        item2.style.borderRadius = 'var(--ui-rad) var(--ui-rad) 0px 0px';
     }
 }
+
+let linkDrop = document.querySelector('.linkdrop img');
+let header1 = document.querySelector('.header1');
+let nav = document.querySelector('div.container-item.nav');
+
+linkDrop.addEventListener('click', () => {
+    let navHide = nav.classList.contains('hide');
+    navHideToggle(nav, navHide);
+    headerRadToggle(header1, navHide);
+});
+
+
+// ------------------------- //
+// SECTION CONTENT HIDE/SHOW //
+// ------------------------- //
 
 function switchIcon(target) {
     if (target.classList.contains('vert-exp') === true) {
@@ -28,46 +43,42 @@ function switchIcon(target) {
     }
 }
 
-function sectionHideToggle(target, content) {
-    let parentSection = content.parentElement;
+function sectionHideToggle(target) {
+    let sectionContent = target.parentElement.parentElement.parentElement.children[1];
+    let parentSection = sectionContent.parentElement;
     let sectionHeader = parentSection.children[0];
     if (target.classList.contains('vert-exp') === true) {
-        content.style.display = 'flex';
+        sectionContent.style.display = 'flex';
         parentSection.style.gap = 'var(--item-padding)';
         parentSection.style.padding = 'var(--item-padding)';
         sectionHeader.style.margin = 'calc(-1 * var(--item-padding)) calc(-1 * var(--item-padding)) 0rem';
     } else if (target.classList.contains('vert-coll') === true) {
-        content.style.display = 'none';
+        sectionContent.style.display = 'none';
         parentSection.style.gap = '0px';
         parentSection.style.padding = '0px';
         sectionHeader.style.margin = '0px';
     }
 }
 
-
-// linkdrop
-
-let linkDrop = document.querySelector('.linkdrop img');
-let header1 = document.querySelector('.header1');
-let nav = document.querySelector('div.container-item.nav');
-
-linkDrop.addEventListener('click', () => {
-    navHideToggle(nav, nav);
-    radiusToggle(nav, header1);
-});
-
-
-// exp/coll secondary section content
+function sectionRadiusToggle(target) {
+    let sectionContent = target.parentElement.parentElement.parentElement.children[1];
+    let parentSection = sectionContent.parentElement;
+    let sectionHeader = parentSection.children[0];
+    
+    if (target.classList.contains('vert-exp') === true) {
+        sectionHeader.style.borderRadius = 'var(--ui-rad) var(--ui-rad) 0px 0px';
+    } else if (target.classList.contains('vert-coll') === true) {
+        sectionHeader.style.borderRadius = '30px';
+    }
+};
 
 let menuIcons = document.querySelectorAll('.menu-icns img');
 
 menuIcons.forEach(icon => icon.addEventListener('click', (e) => {
     let menuTarget = e.target;
-    let sectionContent = menuTarget.parentElement.parentElement.parentElement.children[1];
-    console.log(sectionContent);
     switchIcon(menuTarget);
-    sectionHideToggle(menuTarget, sectionContent);
-    // radiusToggle(e.target);
+    sectionHideToggle(menuTarget);
+    sectionRadiusToggle(e.target);
 }));
 
 
