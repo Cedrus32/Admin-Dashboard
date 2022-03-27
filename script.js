8b51ad938c831df8a11493905b9a1c0128f22a04
Shannon <srosenb@protonmail.ch>
2022-03-27 05:42:25 -0500

    Finish linkdown click functionality

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 // linkdrop
 
-function hideToggle(item) {
-    let itemHide = item.classList.contains('hide');
+function hideToggle(item1, item2) {
+    let itemHide = item1.classList.contains('hide');
     if (itemHide === false) {
-        item.classList.add('hide')
-        console.log(item.classList);
+        item2.classList.add('hide')
+        console.log(item2.classList);
     } else if (itemHide === true) {
-        item.classList.remove('hide');
-        console.log(item.classList);
+        item2.classList.remove('hide');
+        console.log(item2.classList);
+    }
+}
+
+function radiusToggle(item1, item2) {
+    let itemHide = item1.classList.contains('hide');
+    if (itemHide === false) {
+        item2.style.borderRadius = 'var(--ui-rad) var(--ui-rad) 0px 0px';
+    } else if (itemHide === true) {
+        item2.style.borderRadius = 'var(--ui-rad)';
     }
 }
 
 let linkDrop = document.querySelector('.linkdrop img');
 let header1 = document.querySelector('.header1');
-let nav = document.querySelector('nav');
+let nav = document.querySelector('div.container-item.nav');
 
 linkDrop.addEventListener('click', () => {
-    console.log(nav.classList);
-    hideToggle(nav);
+    hideToggle(nav, nav);
+    radiusToggle(nav, header1);
 });
 
 
