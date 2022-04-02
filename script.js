98b224308843501cd6d2e57d7b47036afee73f9c
Shannon <69023504+Cedrus32@users.noreply.github.com>
2022-04-02 11:41:50 -0500

    Fix auto resize bug on section collapse & expand

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function navHideToggle(item) {
 
 let linkDrop = document.querySelector('.linkdrop img');
 let nav = document.querySelector('div.container-item.nav');
-
 linkDrop.addEventListener('click', () => {
     navHideToggle(nav);
 });
@@ -23,6 +22,14 @@ linkDrop.addEventListener('click', () => {
 // ------------------------- //
 // SECTION CONTENT HIDE/SHOW //
 // ------------------------- //
+
+function setInitHeight(target) {
+    let sectionContent = target.parentElement.parentElement.parentElement.children[1];
+    let sectionHeight = sectionContent.clientHeight;
+    if (sectionHeight > 1) {
+        sectionContent.style.height = sectionHeight + 'px';
+    }
+}
 
 function switchHeaderIcon(target) {
     if (target.classList.contains('vert-exp') === true) {
@@ -34,24 +41,16 @@ function switchHeaderIcon(target) {
     }
 }
 
-function setInitHeight(target) {
-    let sectionContent = target.parentElement.parentElement.parentElement.children[1];
-    let sectionHeight = sectionContent.clientHeight;
-    if (sectionHeight > 1) {
-        sectionContent.style.height = sectionHeight + 'px';
-    }
-}
-
 function getHeight(target) {
     let sectionContent = target.parentElement.parentElement.parentElement.children[1];
     let sectionHeight = sectionContent.clientHeight;
-    console.log(sectionHeight); //* working */
+    // console.log(sectionHeight); //* working */
     if (sectionHeight === 1) {
         let height = sectionContent.scrollHeight + 'px';
         return height;
     } else if (sectionHeight > 1) {
         let height = '1px';
-        console.log(height); //* working */
+        // console.log(height); //* working */
         return height;
     }
 }
@@ -61,22 +60,56 @@ function sectionHideToggle(target, sectionHeight) {
     let sectionContent = target.parentElement.parentElement.parentElement.children[1];
     if (target.classList.contains('vert-exp') === true) {
         sectionContent.style.height = sectionHeight;
-        console.log("it's getting bigger");
+        // console.log("it's getting bigger");
+        setTimeout(resetHeight, 200, sectionContent);
     } else if (target.classList.contains('vert-coll') === true) {
         sectionContent.style.height = sectionHeight;
-        console.log("it's getting smaller");
+        // console.log("it's getting smaller");
     }
 }
 
-let menuIcons = document.querySelectorAll('.menu-icns img');
+function resetHeight(target) {
+    target.style.setProperty('height', 'auto');
+}
 
+let menuIcons = document.querySelectorAll('.menu-icns img');
 menuIcons.forEach(icon => icon.addEventListener('click', (e) => {
     let menuTarget = e.target;
-    switchHeaderIcon(menuTarget);
     setInitHeight(menuTarget);
+    switchHeaderIcon(menuTarget);
     let height = getHeight(menuTarget);
     sectionHideToggle(menuTarget, height);
 }));
+
+
+// ---------------------- //
+// GET TRANSITION LENGTHS //
+// ---------------------- //
+
+// function getTransLength(item) {
+//     let navHeight = parseInt(window.getComputedStyle(nav)['marginTop']) * -1;
+//     let length = (navHeight / 250) + 's';
+
+//     console.log(item);
+//     console.log(navHeight);
+//     console.log(length);
+
+//     if (item.classList.contains('nav')) {
+//         item.style.setProperty('--nav-seconds', length);
+//     } else {
+//         let parentSection = item.parentElement.parentElement.parentElement;
+//         let parentContent = parentSection.children[1];
+
+//         if (parentSection.classList.contains('announce')) {
+//             parentContent.style.setProperty('--announce-seconds', length);
+//         } else if (parentSection.classList.contains('trend')) {
+//             parentContent.style.setProperty('--trend-seconds', length);
+//         }
+//     }
+// }
+
+// getTransLength(nav);
+// menuIcons.forEach(icon => getTransLength(icon));
 
 
 ////   background fill
